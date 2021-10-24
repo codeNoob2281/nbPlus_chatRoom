e9e538cb9f4cd6e319e57934477f5ff0c11a4474
codeNoob2281 <2281461058@qq.com>
2021-10-24 13:06:27 +0800

    ee

---
+++ resources/js/login.js
@@ -0,0 +1,14 @@
+
+
+
+//登录表单校验
+function formValidation(){
+    if($('#username').val()===''){
+        alert('账号不能为空');
+        return false;
+    }
+    return true;
+}
+
+
+
---
+++ resources/js/register.js
@@ -0,0 +1,14 @@
+
+
+//注册表单校验
+function regValidation(){
+    if($('#username').val()===""){
+        alert('账号不能为空');
+        return false;
+    }else if($('#password').val()!==$('#repeatPsw').val()){
+        alert('两次密码输入不一致');
+        return false;
+    }else {
+        return true;
+    }
+}
